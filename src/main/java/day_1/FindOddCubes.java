70fb8e78b3f7a5d112fa704fe654dd54dc70f146
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-04-19 14:18:14 +0100

    Commiting

---
+++ src/main/java/day_1/FindOddCubes.java
@@ -0,0 +1,23 @@
+package day_1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Find the sum of the odd numbers within an array, after cubing the initial integers.
+The function should return undefined/None/nil/NULL if any of the values aren't numbers.
+
+Note: There are ONLY integers in the JAVA and C# versions of this Kata.
+ */
+public class FindOddCubes {
+    public static void main(String[] args) {
+        System.out.println(cubeOdd(new int[] {1, 2, 3, 4}));
+    }
+    public static int cubeOdd(int[] arr) {
+        return Arrays.stream(arr)
+                .map(n -> n*n*n)
+                .filter(n -> n % 2 != 0)
+                .sum();
+    }
+}
